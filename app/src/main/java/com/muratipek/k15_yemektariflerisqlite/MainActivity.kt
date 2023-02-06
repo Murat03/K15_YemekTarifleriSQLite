2852c611b4aa7afc73f25db3787ed4a393f8287e
Murat03 <muratipekk033@gmail.com>
2023-02-06 14:08:16 +0300

    I started new project and added a selectImage

---
+++ app/src/main/java/com/muratipek/k15_yemektariflerisqlite/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.muratipek.k15_yemektariflerisqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
